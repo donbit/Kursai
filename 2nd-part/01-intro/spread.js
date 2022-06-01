ff0c560199e8b07cde57b1891f2856a283ecb64b
donbit <100865147+donbit@users.noreply.github.com>
2022-06-01 20:18:40 +0300

    server

---
+++ 2nd-part/01-intro/spread.js
@@ -0,0 +1,16 @@
+
+
+
+const a = [1, 2];
+console.log('A:', a);
+
+const b = a;
+console.log('B:',b);
+
+b[0] = 11;
+console.log('B:',b);
+
+b[1] = 22;
+console.log('B:',b);
+
+console.log('A:', a);
