d3e984c0e7fe4fb34fb7bc338821c7f21d5577b5
donbit <100865147+donbit@users.noreply.github.com>
2022-06-09 21:04:49 +0300

    server

--- 2nd-part/02-server/index.js
+++ 2nd-part/02-server/index.js
@@ -1,18 +1,42 @@
-import { server } from './server.js';
-console.log('hi')
-const config = {};
+import { server } from './lib/server.js';
 
-config.dev = {
-    name: 'dev',
+const app = {};
+
+app.init = () => {
+    // susikurti busimus reikiamus folder'ius
+    // susikurti busimus reikiamus failus, pvz.: robots.txt
+
+    // prisijungti prie duomenu bazes (DB)
+
+    // paleidziam pacio serverio logika (perduodame prisijungima prie DB)
+    server.init();
+
+    // paleidzia papildomus nuolatinius procesus:
+    // - issitrinti nebereikalingus failus
+    // - suarchivuoti retai naudojamus failus
+    // - prasukti reikiamus API (3rd-party info, pvz.: valiutu kursai, orai)
+    // - pasitikrinti, jog vartotojai, kurie neturi teises buti prisijunge, jog ir nera prisijunge, o jei yra - atjungiame!!!
 }
 
-config.prod = {
-    name: 'prod',
-}
+app.init();
 
-const nodeENV = process.env.NODE_ENV;
-const env = nodeEnv ? nodeEnv : 'dev';
-const options = config[env] ? config[env] : config.dev;
+export default app;
+
+// import { server } from './lib/server.js';
+
+// const config = {};
+
+// config.dev = {
+//     name: 'dev',
+// }
+
+// config.prod = {
+//     name: 'prod',
+// }
+
+// const nodeENV = process.env.NODE_ENV;
+// const env = nodeEnv ? nodeEnv : 'dev';
+// const options = config[env] ? config[env] : config.dev;
 
 
 
